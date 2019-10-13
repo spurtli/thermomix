19f2a1df9b7e7644a01126bcb7dbce83e4652df6
Neele Barthel <neele.barthel@shopify.com>
2019-10-13 17:43:14 -0400

    feat: 🎸 add user provider and matching hook

---
+++ packages/react-oauth-provider/src/user-provider/provider.tsx
@@ -0,0 +1,12 @@
+import React, {ReactNode} from "react";
+import {UserContext} from "./context";
+import {UserContextType} from "./types";
+
+interface Props<T> {
+  children: ReactNode;
+  user: UserContextType<T>;
+}
+
+export function UserProvider<T>({children, user}: Props<T>) {
+  return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+}
