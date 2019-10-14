8c5fa19c78962d243bdb3dbf44f7388cea2f3b3a
Neele Barthel <neele.barthel@shopify.com>
2019-10-14 14:16:41 -0400

    style: 💄 move prettier config to root

--- packages/react-oauth-provider/rollup.config.ts
+++ packages/react-oauth-provider/rollup.config.ts
@@ -1,25 +1,25 @@
 // @ts-ignore
-import babel from "rollup-plugin-babel";
+import babel from 'rollup-plugin-babel';
 // @ts-ignore
-import camelCase from "lodash.camelcase";
+import camelCase from 'lodash.camelcase';
 // @ts-ignore
-import commonjs from "rollup-plugin-commonjs";
+import commonjs from 'rollup-plugin-commonjs';
 // @ts-ignore
-import resolve from "rollup-plugin-node-resolve";
+import resolve from 'rollup-plugin-node-resolve';
 // @ts-ignore
-import sourceMaps from "rollup-plugin-sourcemaps";
+import sourceMaps from 'rollup-plugin-sourcemaps';
 // @ts-ignore
-import typescript from "rollup-plugin-typescript2";
+import typescript from 'rollup-plugin-typescript2';
 // @ts-ignore
-import builtins from "rollup-plugin-node-builtins";
+import builtins from 'rollup-plugin-node-builtins';
 
-const pkg = require("./package.json");
+const pkg = require('./package.json');
 const dependencies = [
   ...Object.keys(pkg.dependencies),
   ...Object.keys(pkg.peerDependencies)
 ];
 
-const libraryName = "react-oauth-provider";
+const libraryName = 'react-oauth-provider';
 
 export default {
   input: `src/index.ts`,
@@ -27,31 +27,31 @@ export default {
     {
       file: pkg.main,
       name: camelCase(libraryName),
-      format: "umd",
+      format: 'umd',
       sourcemap: true,
       globals: {
-        moment: "moment",
-        react: "React",
-        events: "EventEmitter",
-        qs: "qs"
+        moment: 'moment',
+        react: 'React',
+        events: 'EventEmitter',
+        qs: 'qs'
       }
     },
-    {file: pkg.module, format: "es", sourcemap: true}
+    {file: pkg.module, format: 'es', sourcemap: true}
   ],
   external: dependencies,
   watch: {
-    include: "src/**/*"
+    include: 'src/**/*'
   },
   plugins: [
     builtins(),
     typescript(),
     babel({
-      extensions: [".ts", ".tsx"],
-      exclude: "node_modules/**"
+      extensions: ['.ts', '.tsx'],
+      exclude: 'node_modules/**'
     }),
     commonjs(),
     resolve({
-      extensions: [".ts", ".tsx"],
+      extensions: ['.ts', '.tsx'],
       browser: true
     }),
     sourceMaps()
