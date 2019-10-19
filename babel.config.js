e0dba63c617f70834c365c983329242e5716b5b5
Neele Barthel <neele.barthel@shopify.com>
2019-10-19 17:42:05 -0400

    style: 💄 format babel config

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   presets: [
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
-        targets: { browsers: [">1.00%", "not ie 11", "not op_mini all"] }
+        targets: {browsers: ['>1.00%', 'not ie 11', 'not op_mini all']}
       }
     ],
-    "@babel/preset-typescript",
-    "@babel/preset-react"
+    '@babel/preset-typescript',
+    '@babel/preset-react'
   ],
-  plugins: ["@babel/plugin-proposal-class-properties"]
+  plugins: ['@babel/plugin-proposal-class-properties']
 };
