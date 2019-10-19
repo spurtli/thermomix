90d15069a9c00b633a59643f3c67ef2dc7dff052
Neele Barthel <neele.barthel@shopify.com>
2019-10-19 17:47:27 -0400

    fix: 🐛 update types and imports

--- packages/react-oauth-provider/src/index.ts
+++ packages/react-oauth-provider/src/index.ts
@@ -2,5 +2,5 @@ export {useProtection} from './hook';
 export {context} from './context';
 export {AuthProvider} from './provider';
 export {Provider as OAuthProvider} from './types';
-export {AuthEvent} from './service';
-export {Record} from './storage';
+export {AuthEvent, Service} from './service';
+export {Record, create} from './storage';
